c8a7148fac96cb84027d7b4631d55477c7be29be
Jose Abundez <Jabundez@csu.fullerton.edu>
2016-04-29 15:12:33 -0700

    constants.h

---
+++ constants.h
@@ -0,0 +1,17 @@
+/* ----------------------------------------------------------------------------
+* Copyright & copy; 2016 Jose Abundez <Jabundez@csu.fullerton.edu>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
+
+/**
+* A file for holding constants.
+*/
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H	
+
+//Terminal Size
+const int X = 79;
+const int Y = 25;
+
+#endif
